ffa144375d590df5bfbfb657391c76dc8db360fe
felixjaschul <felix@jaschul.de>
2026-04-08 00:02:09 +0200

    win fix

--- Engine/opengl.h
+++ Engine/opengl.h
@@ -4,9 +4,9 @@
     #include <OpenGL/gl3.h>
     #include <OpenGL/glext.h>
 #elif defined(_WIN32)
-    #define WIN32_LEAN_AND_MEAN
-    #include <windows.h>
-    #include <GL/gl.h>
+    #define GLFW_INCLUDE_NONE
+    #define GLFW_INCLUDE_NO_GLU
+    #include <GLFW/glfw3.h>
 #else
     #define GL_GLEXT_PROTOTYPES 1
     #include <GL/gl.h>

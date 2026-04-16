17f40721eee64c7c03fd1a56e5218e2cf42c8efd
felixjaschul <felix@jaschul.de>
2026-04-16 12:25:06 +0200

    .

---
+++ Engine/portal.c
@@ -0,0 +1,6 @@
+#include "portal.h"
+
+void render_quad(level_quad_t *b, level_quad_t *a) 
+{
+    /* is called inside level_render if there are to quads withe the same portal id, these are then inputted into the portal_render() function instead of render_quad */ 
+}
---
+++ Engine/portal.h
@@ -0,0 +1,8 @@
+#ifndef PORTAL_H
+#define PORTAL_H
+
+#include "level.h"
+
+void render_portal(level_quad_t *a, level_quad_t *b);
+
+#endif // PORTAL_H

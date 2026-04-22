fef8a3e3647035fa6e38dd1479acd3b232073af7
felixjaschul <felix@jaschul.de>
2026-04-23 01:21:43 +0200

    portals YIPPI

--- Engine/res/level1.h
+++ Engine/res/level1.h
@@ -122,7 +122,7 @@ static inline level_data_t load_1(void)
         .path = "level1.h",
         .sectors = level1_sectors,
         .sector_count = sizeof(level1_sectors) / sizeof(level1_sectors[0]),
-        .cam = { .pos = {9.119f, -5.500f, 20.276f}, .yaw = -8196.379f, .pitch = -1.776f }
+        .cam = { .pos = {6.485f, -5.500f, 12.433f}, .yaw = -8292.573f, .pitch = 5.524f }
     };
 }
 

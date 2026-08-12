b553470b42efe4633528e3bb22daecd0ca6c8536
Felix <felix@xt9y.de>
2026-08-12 18:42:08 +0200

    Migrate build.c to compiler-only c API

--- build.c
+++ build.c
@@ -1,26 +1,85 @@
 #include <cbuild.h>
 
+static void add_imgui(C_Target *app) {
+    c_sources(app, "Vendor/imgui/imgui.cpp");
+    c_sources(app, "Vendor/imgui/imgui_demo.cpp");
+    c_sources(app, "Vendor/imgui/imgui_draw.cpp");
+    c_sources(app, "Vendor/imgui/imgui_tables.cpp");
+    c_sources(app, "Vendor/imgui/imgui_widgets.cpp");
+    c_sources(app, "Vendor/imgui/imgui_impl_glfw.cpp");
+    c_sources(app, "Vendor/imgui/imgui_impl_opengl3.cpp");
+}
+
+static void add_glfw(C_Target *app) {
+    c_sources(app, "Vendor/glfw/src/context.c");
+    c_sources(app, "Vendor/glfw/src/init.c");
+    c_sources(app, "Vendor/glfw/src/input.c");
+    c_sources(app, "Vendor/glfw/src/monitor.c");
+    c_sources(app, "Vendor/glfw/src/platform.c");
+    c_sources(app, "Vendor/glfw/src/vulkan.c");
+    c_sources(app, "Vendor/glfw/src/window.c");
+    c_sources(app, "Vendor/glfw/src/egl_context.c");
+    c_sources(app, "Vendor/glfw/src/osmesa_context.c");
+    c_sources(app, "Vendor/glfw/src/null_init.c");
+    c_sources(app, "Vendor/glfw/src/null_monitor.c");
+    c_sources(app, "Vendor/glfw/src/null_window.c");
+    c_sources(app, "Vendor/glfw/src/null_joystick.c");
+
+#ifdef __APPLE__
+    c_define(app, "_GLFW_COCOA");
+    c_sources(app, "Vendor/glfw/src/macos_time.c");
+    c_sources(app, "Vendor/glfw/src/posix_module.c");
+    c_sources(app, "Vendor/glfw/src/posix_thread.c");
+    c_sources(app, "Vendor/glfw/src/cocoa_init.m");
+    c_sources(app, "Vendor/glfw/src/cocoa_joystick.m");
+    c_sources(app, "Vendor/glfw/src/cocoa_monitor.m");
+    c_sources(app, "Vendor/glfw/src/cocoa_window.m");
+    c_sources(app, "Vendor/glfw/src/nsgl_context.m");
+#else
+    c_define(app, "_GLFW_X11");
+    c_sources(app, "Vendor/glfw/src/posix_time.c");
+    c_sources(app, "Vendor/glfw/src/posix_module.c");
+    c_sources(app, "Vendor/glfw/src/posix_thread.c");
+    c_sources(app, "Vendor/glfw/src/x11_init.c");
+    c_sources(app, "Vendor/glfw/src/x11_monitor.c");
+    c_sources(app, "Vendor/glfw/src/x11_window.c");
+    c_sources(app, "Vendor/glfw/src/xkb_unicode.c");
+    c_sources(app, "Vendor/glfw/src/glx_context.c");
+    c_sources(app, "Vendor/glfw/src/linux_joystick.c");
+    c_sources(app, "Vendor/glfw/src/posix_poll.c");
+#endif
+}
+
 void build(C_Build *b) {
     C_Target *app = c_executable(b, "bge");
+
     c_sources(app, "main.c");
+    c_sources(app, "Engine/App.c");
+    c_sources(app, "Engine/cam.c");
+    c_sources(app, "Engine/gfx.c");
+    c_sources(app, "Engine/text.c");
+    c_sources(app, "Engine/level.c");
+    c_sources(app, "Engine/portal.c");
+    c_sources(app, "Engine/editor.c");
+    c_sources(app, "Engine/gun.c");
+    c_sources(app, "Engine/render.c");
+    c_sources(app, "Engine/util/math.c");
+    c_sources(app, "Engine/imgui_c.cpp");
+    c_sources(app, "Vendor/glad/src/glad.c");
+    add_imgui(app);
+    add_glfw(app);
+
     c_include(app, ".");
     c_include(app, "Engine");
     c_include(app, "Vendor/glad/include");
     c_include(app, "Vendor/glfw/include");
+    c_include(app, "Vendor/glfw/src");
     c_include(app, "Vendor/imgui");
-    c_define(app, "GL_SILENCE_DEPRECATION");
 
-    C_Dependency *engine = c_git(
-        b, "bge-engine", "https://github.com/xt9y/BGE.git",
-        "main"
-    );
-    c_dep_cmake(engine);
-    c_dep_subdir(engine, "Engine");
-    c_dep_cmake_option(engine, "-DGLFW_INSTALL=ON");
-    c_dep_link(engine, "engine");
-    c_dep_link(engine, "imgui");
-    c_dep_link(engine, "glfw3");
-    c_use(app, engine);
+    c_define(app, "GL_SILENCE_DEPRECATION");
+    c_flag(app, "-std=c++11");
+    c_flag(app, "-Wall");
+    c_flag(app, "-Wextra");
 
 #ifdef __APPLE__
     c_link_system(app, "c++");

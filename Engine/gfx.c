471be983111910ef681cd6bcf24b7c429e5b56b6
Felix <felix@xt9y.de>
2026-08-13 15:21:38 +0200

    Fail cleanly on shader errors

--- Engine/gfx.c
+++ Engine/gfx.c
@@ -4,25 +4,61 @@
 
 u32 compile_shader(const u32 type, const char* src)
 {
-    const u32 s = glCreateShader(type);
-    glShaderSource(s,1,&src,0);
-    glCompileShader(s);
-    i32 ok; glGetShaderiv(s,GL_COMPILE_STATUS,&ok);
-    if (!ok) { 
-        char log[512]; 
-        glGetShaderInfoLog(s,512,0,log); 
-        printf("Shader error: %s\n",log);
-    }    
-    return s;
+    if (!src) return 0;
+
+    const u32 shader = glCreateShader(type);
+    if (!shader) return 0;
+
+    glShaderSource(shader, 1, &src, NULL);
+    glCompileShader(shader);
+
+    i32 ok = GL_FALSE;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &ok);
+    if (!ok) {
+        char log[2048] = {0};
+        glGetShaderInfoLog(shader, (GLsizei)sizeof(log), NULL, log);
+        fprintf(stderr, "BGE: shader compilation failed: %s\n", log);
+        glDeleteShader(shader);
+        return 0;
+    }
+
+    return shader;
 }
 
 u32 create_program(const char* vs, const char* fs)
 {
-    const u32 v = compile_shader(GL_VERTEX_SHADER,vs);
-    const u32 f = compile_shader(GL_FRAGMENT_SHADER,fs);
-    const u32 p = glCreateProgram();
-    glAttachShader(p,v); glAttachShader(p,f); glLinkProgram(p);
-    glDeleteShader(v); glDeleteShader(f);
-    return p;
-}
+    const u32 vertex = compile_shader(GL_VERTEX_SHADER, vs);
+    if (!vertex) return 0;
 
+    const u32 fragment = compile_shader(GL_FRAGMENT_SHADER, fs);
+    if (!fragment) {
+        glDeleteShader(vertex);
+        return 0;
+    }
+
+    const u32 program = glCreateProgram();
+    if (!program) {
+        glDeleteShader(vertex);
+        glDeleteShader(fragment);
+        return 0;
+    }
+
+    glAttachShader(program, vertex);
+    glAttachShader(program, fragment);
+    glLinkProgram(program);
+
+    glDeleteShader(vertex);
+    glDeleteShader(fragment);
+
+    i32 ok = GL_FALSE;
+    glGetProgramiv(program, GL_LINK_STATUS, &ok);
+    if (!ok) {
+        char log[2048] = {0};
+        glGetProgramInfoLog(program, (GLsizei)sizeof(log), NULL, log);
+        fprintf(stderr, "BGE: shader program link failed: %s\n", log);
+        glDeleteProgram(program);
+        return 0;
+    }
+
+    return program;
+}

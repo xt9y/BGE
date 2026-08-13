cf981e7802a3559a8b34bb1c5467320ebc1fce3e
Felix <felix@xt9y.de>
2026-08-13 15:12:54 +0200

    Expose single-level editor lifecycle

--- Engine/editor.h
+++ Engine/editor.h
@@ -30,22 +30,21 @@ typedef struct {
 } editor_wall_window_t;
 
 typedef struct {
-    level_data_t *level;
-    level_quad_t *selected_quad;
-    level_sector_data_t *selected_sector;
+    level_data_t* level;
+    level_quad_t* selected_quad;
+    level_sector_data_t* selected_sector;
     i32 selected_wall_idx;
-    
+
     editor_e id;
     editor_e hover_id;
-    
+
     vec3s drag_start_hit,
           drag_quad_start_pos,
           drag_quad_start_rot,
           drag_plane_normal,
           drag_cam_start_pos;
-
     vec2s drag_quad_start_size;
-    
+
     level_quad_t template_quad;
     u32 template_mods;
 
@@ -67,27 +66,37 @@ typedef struct {
     level_sector_data_t* sector;
 } editor_look_at_info_t;
 
-void editor_render();
-void editor_render_borders();
-void editor_update();
-void editor_save(level_data_t* level);
-
-void editor_ui();
-void editor_add_level();
-void editor_switch_level(i32 idx);
+void editor_init(void);
+void editor_shutdown(void);
+void editor_render(void);
+void editor_render_borders(void);
+void editor_update(void);
+void editor_ui(void);
 void editor_open_wall_window(i32 sector_id, i32 quad_idx);
 
-void editor_add_quad(level_sector_data_t* sector, const level_quad_t* template);
+void editor_add_quad(level_sector_data_t* sector, const level_quad_t* template_quad);
 void editor_delete_quad(level_sector_data_t* sector, i32 idx);
 void editor_add_sector(level_data_t* level);
 void editor_move_quad_to_sector(level_sector_data_t* from, level_sector_data_t* to, i32 idx);
 int count_portal_quads(level_data_t* level, int portal_id);
 
-static level_quad_t get_default_quad(camera_t *cam) 
+static level_quad_t get_default_quad(camera_t* cam)
 {
-    return (level_quad_t) { 
-        .pos = {roundf(cam->pos.x + cam->front.x * 3.0f), roundf(cam->pos.y + cam->front.y * 3.0f), roundf(cam->pos.z + cam->front.z * 3.0f)}, 
-        .rot = {0, 0, 0}, .size = {2, 2}, .tex_id = 0, .color = {1, 1, 1}, .is_solid = true, .is_invisible = false, .is_billboard = false, .portal_side_flip = false, .sector_id = 0
+    return (level_quad_t){
+        .pos = {
+            roundf(cam->pos.x + cam->front.x * 3.0f),
+            roundf(cam->pos.y + cam->front.y * 3.0f),
+            roundf(cam->pos.z + cam->front.z * 3.0f)
+        },
+        .rot = {0, 0, 0},
+        .size = {2, 2},
+        .tex_id = 0,
+        .color = {1, 1, 1},
+        .is_solid = true,
+        .is_invisible = false,
+        .is_billboard = false,
+        .portal_side_flip = false,
+        .sector_id = 0
     };
 }
 
